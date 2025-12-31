a715b556ac3c3b3b63ddabe53cdd67540bf759ed
Mecha1028 <thomasmillership1@icloud.com>
2025-12-31 15:48:23 +0000

    Initial commit

---
+++ Source/VerticalSliceProject/VerticalSliceProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VerticalSliceProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VerticalSliceProject, "VerticalSliceProject" );
