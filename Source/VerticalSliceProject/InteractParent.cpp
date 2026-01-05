30000f321c629361d2abd98151e0443136cd9127
Mecha1028 <119541883+Mecha1028@users.noreply.github.com>
2026-01-05 15:21:04 +0000

    Added AI and Interact Parent

--- Source/VerticalSliceProject/InteractParent.cpp
+++ Source/VerticalSliceProject/InteractParent.cpp
@@ -6,9 +6,7 @@
 // Sets default values
 AInteractParent::AInteractParent()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
@@ -16,12 +14,5 @@ void AInteractParent::BeginPlay()
 {
 	Super::BeginPlay();
 	
-}
-
-// Called every frame
-void AInteractParent::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
 }
 
